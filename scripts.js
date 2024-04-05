f68ed52447ddd3d9d6897db5af8cd68cbd67247b
Cam <25141441+keyman015@users.noreply.github.com>
2024-04-05 21:18:47 +0800

    Add bassic password checking functions to script
    - Pass some of the parsing to client-side to reduce server traffic
      for invalid attempts.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,43 @@
 // Set verbosity
 const verbose = true;
 
+// Password requirements (global so only gotten once)
+let passRequirements = null;
+
 // Wait for page load and add listener event for form submission:
 document.addEventListener('DOMContentLoaded', () => {
-  if (verbose) { console.log("Page loaded!") }
+  if (verbose) { console.log("Page loaded!"); }
+
+  // Get password requirements from server (TO DO LATER)
+  passRequirements = get_passRequirements();
+
 });
+
+// Obtain current password requirements from server
+function get_passRequirements() {
+  return {
+    'maxLength': 10,
+    'minLength': 5,
+    'regex': '^[a-zA-Z0-9?_!-]*$'
+  };
+}
+
+// Ensure password submitted has the correct requirements
+function check_passRequirements(pass) {
+  // Ensure password requirements are obtained correctly.
+  if (passRequirements === null || Object.keys(passRequirements).length > 1) {
+    console.log("Password requirements incorrectly set!");
+    return false; 
+  }
+
+  // Ensure correct password length
+  if (pass.length > passRequirements['minLength'] && pass.length < passRequirements['maxLength']) {
+    // Check against current password regex requirements
+    const regex = new RegExp(passRequirements['regex']);
+    if (regex.test(pass)) {
+      return true;
+    }
+  }
+
+  return false;
+}
