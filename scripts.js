5513bc833981da159fe79f6b9c3efd13b31351f0
Cam <25141441+keyman015@users.noreply.github.com>
2024-04-05 20:56:32 +0800

    Add listner for page load event and verbosity
    - Global verbosity level for javascript file to help in debugging.

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Set verbosity
+const verbose = true;
+
+// Wait for page load and add listener event for form submission:
+document.addEventListener('DOMContentLoaded', () => {
+  if (verbose) { console.log("Page loaded!") }
+});
