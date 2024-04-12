4c882f27bc671de23626051caa3d7073b6df9b0f
Stefan <stefanandonov03@gmail.com>
2024-04-12 15:38:06 +0800

    Added email field for account creation upon button click

--- log-in.js
+++ log-in.js
@@ -1,6 +1,14 @@
 const createAccountButton = document.getElementById("create-account-button");
 createAccountButton.addEventListener("click", handleAccount);
 
+//will add an email field to the form, change action value of the form, change legend
 function handleAccount() {
-    
+
+    let loginForm = document.getElementById("login-form");
+
+    let emailLabel = document.getElementById("email-label");
+    let emailInput = document.getElementById("email-input");
+
+    emailLabel.classList.remove("hidden");
+    emailInput.classList.remove("hidden");
 }
