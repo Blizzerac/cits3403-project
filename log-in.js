f879ad409dca773122a28e779f2cd0bcc27abdee
Stefan <stefanandonov03@gmail.com>
2024-04-12 15:43:10 +0800

    Account creation now changes action value of form

--- log-in.js
+++ log-in.js
@@ -5,6 +5,7 @@ createAccountButton.addEventListener("click", handleAccount);
 function handleAccount() {
 
     let loginForm = document.getElementById("login-form");
+    loginForm.setAttribute("action", "/submit_new_account");
 
     let emailLabel = document.getElementById("email-label");
     let emailInput = document.getElementById("email-input");
