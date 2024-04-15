3406b17f073a4704de30ce70510bb43ffcd32a04
Stefan <stefanandonov03@gmail.com>
2024-04-15 08:56:54 +0800

    generalised hiding and showing inputs, added confirm password field to form

--- log-in.js
+++ log-in.js
@@ -5,23 +5,27 @@ changeButton.addEventListener("click", toggleForm);
 // changes between account creation and login forms using the button id
 function toggleForm() {
     const creatingAccount = (changeButton.id === "create-account-button");
-
     const loginForm = document.getElementById("login-form");
-    const emailLabel = document.getElementById("email-label");
-    const emailInput = document.getElementById("email-input");
     const legend = document.getElementById("legend");
+    const hiddenElements = document.getElementsByClassName("account-creation");
 
     if (creatingAccount) {
         loginForm.setAttribute("action", "/submit_new_account");
-        emailLabel.classList.remove("hidden");
-        emailInput.classList.remove("hidden");
+
+        for (let i = 0; i < hiddenElements.length; i++) {
+            hiddenElements[i].classList.remove("hidden");
+        }
+
         legend.textContent = "Account Creation";
-        changeButton.textContent = "Back to login";
+        changeButton.textContent = "Back to Login";
         changeButton.id = "login-button";
     } else {
         loginForm.setAttribute("action", "/submit_login");
-        emailLabel.classList.add("hidden");
-        emailInput.classList.add("hidden");
+
+        for (let i = 0; i < hiddenElements.length; i++) {
+            hiddenElements[i].classList.add("hidden");
+        }
+
         legend.textContent = "Login";
         changeButton.textContent = "Create Account";
         changeButton.id = "create-account-button";
