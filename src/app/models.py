ec0446a188325fcd1baf7d3005b81603d65be281
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-04-21 22:42:34 +0800

    Added email field to database

--- src/app/models.py
+++ src/app/models.py
@@ -6,6 +6,7 @@ from datetime import datetime
 class User(db.Model, UserMixin):
   userID = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(20), unique=True, nullable=False) # UNIQUE username
+  email = db.Column(db.String(80), nullable=False)
   password = db.Column(db.String(80), nullable=False)
   creationDate = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) # User account creation date
 
