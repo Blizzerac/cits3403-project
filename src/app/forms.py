d390f81935c840dca0e8f98506936f092a024233
Stefan <stefanandonov03@gmail.com>
2024-04-24 21:21:23 +0800

    Made forms.py more readable, added image for post-form and some typo fixes

--- src/app/forms.py
+++ src/app/forms.py
@@ -14,20 +14,41 @@ def username_or_email(form, field):
         Length(min=4, max=20)(form, field)
 
 class SignupForm(FlaskForm):
-  username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-  email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email address.')], render_kw={"placeholder": "Email"})
-  password = PasswordField('Password', validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Password"})
-  submit = SubmitField("Register")
-
-  def validate_username(self, username):
-   existing_user = User.query.filter_by(username=username.data).first()
-   if existing_user:
-      raise ValidationError("That username already exists. Please choose a different one.")
+    username = StringField(
+        'Username', 
+        validators=[InputRequired(), Length(min=4, max=20)], 
+        render_kw={"placeholder": "Username"})
+    
+    email = StringField(
+        'Email', 
+        validators=[InputRequired(), Email(message='Invalid email address.')], 
+        render_kw={"placeholder": "Email"})
+    
+    password = PasswordField(
+        'Password', 
+        validators=[InputRequired(), Length(min=5, max=25)], 
+        render_kw={"placeholder": "Password"})
+    
+    submit = SubmitField("Register")
+    
+    def validate_username(self, username):
+        existing_user = User.query.filter_by(username=username.data).first()
+        if existing_user:
+            raise ValidationError("That username already exists. Please choose a different one.")
 
 class LoginForm(FlaskForm):
-    login = StringField('Username or Email', validators=[InputRequired(), username_or_email], render_kw={"placeholder": "Username or Email"})
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Password"})
+    login = StringField(
+        'Username or Email', 
+        validators=[InputRequired(), username_or_email], 
+        render_kw={"placeholder": "Username or Email"})
+
+    password = PasswordField(
+        'Password', 
+        validators=[InputRequired(), Length(min=5, max=25)], 
+        render_kw={"placeholder": "Password"})
+
     remember_me = BooleanField('Remember me')
+    
     submit = SubmitField("Login")
 
 class PostForm(FlaskForm):
