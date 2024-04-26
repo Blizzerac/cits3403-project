6194178e51e13a41bdf3da5602da66b9647d9c53
UpstandingVlad <164867745+UpstandingVlad@users.noreply.github.com>
2024-04-27 00:58:30 +0800

    changed leaderboard table to receive the user information from the flask backend, added a stub of hardcoded example users for testing purposes

--- src/app/routes.py
+++ src/app/routes.py
@@ -8,6 +8,32 @@ from app import app
 ###########
 # Please rememeber that the last app.route tag is what the page will be displayed as.
 
+# Hardcoded user data for leaderboard testing purposes
+leaderboard_users = [
+    {"username": "User 1", "quests_complete": 10},
+    {"username": "User 2", "quests_complete": 8},
+    {"username": "User 3", "quests_complete": 7},
+    {"username": "User 4", "quests_complete": 6},
+    {"username": "User 5", "quests_complete": 5},
+    {"username": "User 6", "quests_complete": 4},
+    {"username": "User 7", "quests_complete": 3},
+    {"username": "User 8", "quests_complete": 2},
+    {"username": "User 9", "quests_complete": 1},
+    {"username": "User 10", "quests_complete": 0},
+    {"username": "User 11", "quests_complete": 0},
+    {"username": "User 12", "quests_complete": 0},
+    {"username": "User 13", "quests_complete": 0},
+    {"username": "User 14", "quests_complete": 0},
+    {"username": "User 15", "quests_complete": 0},
+    {"username": "User 16", "quests_complete": 0},
+    {"username": "User 17", "quests_complete": 0},
+    {"username": "User 18", "quests_complete": 0},
+    {"username": "User 19", "quests_complete": 0},
+    {"username": "User 20", "quests_complete": 0}
+]
+
+
+
 # Home page
 @app.route("/index")
 @app.route("/home")
@@ -24,7 +50,7 @@ def login():
 # Leaderboard
 @app.route("/leaderboard")
 def leaderboard():
-    return render_template("leaderboard.html")
+    return render_template("leaderboard.html", users=leaderboard_users)
 
 @app.route("/search")
 def search():
