cfb853c6d3313f06518ff2a1393fce1414c0f156
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-05-03 22:09:14 +0800

    Set flask secret key to be locally dependent
    - For security reasons, future secret keys will no longer be pushed to
      source control.
    - Instructions:
      --Set local secret key with:
          BASH: echo 'export FLASK_SECRET_KEY="your_secret_key_here"' >> ~/.bashrc && source ~/.bashrc
          WINDOWS CMD: set FLASK_SECRET_KEY=your_secret_key_here

--- src/app/config.py
+++ src/app/config.py
@@ -4,4 +4,7 @@ import os
 class Config(object):
   SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL') or 'sqlite:///../instance/database.db'
   SQLALCHEMY_TRACK_MODIFICATION = False
-  SECRET_KEY = 'placeholder' ### CHANGE THIS!
+  SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
+  # Set local secret key with
+  # BASH: echo 'export FLASK_SECRET_KEY="your_secret_key_here"' >> ~/.bashrc && source ~/.bashrc
+  # WINDOWS CMD: set FLASK_SECRET_KEY=your_secret_key_here
