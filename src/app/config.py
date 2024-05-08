8ac17e5e576cddb67d6a2d907a0a63c5db625b26
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-05-08 14:58:10 +0800

    Fix configuration typo

--- src/app/config.py
+++ src/app/config.py
@@ -3,7 +3,7 @@ import os
 # Configuration class
 class Config(object):
   SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL') or 'sqlite:///../instance/database.db'
-  SQLALCHEMY_TRACK_MODIFICATION = False
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
   SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
   # Set local secret key with
   # BASH: echo 'export FLASK_SECRET_KEY="your_secret_key_here"' >> ~/.bashrc && source ~/.bashrc
