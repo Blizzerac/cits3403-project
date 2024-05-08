12449413502bb0f263bac5e21d2a1955662da8ed
Blizzerac <TBlizzerac@hotmail.com>
2024-05-08 15:42:28 +0800

    modified config.py to take data from a .env file in the base directory

--- src/app/config.py
+++ src/app/config.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Configuration class
 class Config(object):
-  SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL') or 'sqlite:///../instance/database.db'
-  SQLALCHEMY_TRACK_MODIFICATION = False
-  SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
+  SQLALCHEMY_DATABASE_URI =  os.getenv('DATABASE_URL') or 'sqlite:///../instance/database.db'
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
   # Set local secret key with
   # BASH: echo 'export FLASK_SECRET_KEY="your_secret_key_here"' >> ~/.bashrc && source ~/.bashrc
   # WINDOWS CMD: set FLASK_SECRET_KEY=your_secret_key_here
