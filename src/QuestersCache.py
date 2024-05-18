7bf2ed4841a2d5f91aa138330e78cffc709134a4
UpstandingVlad <164867745+UpstandingVlad@users.noreply.github.com>
2024-05-19 00:53:21 +0800

    Reverted changes to before bug

--- src/QuestersCache.py
+++ src/QuestersCache.py
@@ -1,12 +1,4 @@
 # Imports
-from app import create_app, db
-from app.config import TestConfig
-from app import models
 from app import models
 
 # Main application code
-
-# Create the Flask app
-print("Creating app with testconfig")
-flaskApp = create_app(TestConfig)
-migrate = Migrate(flaskApp, db)
--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,44 +3,31 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.config import Config, TestConfig
+from app.config import Config
 
-db = SQLAlchemy()
+# Main application name
+flaskApp = Flask(__name__, template_folder='../templates', static_folder='../static')
 
-def create_app(config):
-    # Main application name
-    flaskApp = Flask(__name__, template_folder='../templates', static_folder='../static')
+# Configuration
+flaskApp.config.from_object(Config)
+debug = True # Debug error messages
 
-    # Configuration
-    #flaskApp = Flask(__name__)
-    flaskApp.config.from_object(config)
-    #flaskApp.debug = True # Debug error messages
+# Initialise extensions
+db = SQLAlchemy(flaskApp)
+migrate = Migrate(flaskApp, db)
 
-    db.init_app(flaskApp)
-   
-    
-    # Initialise extensions
-    #db = SQLAlchemy(flaskApp)
-    #migrate = Migrate(flaskApp, db)
-    
-    # Login manager
-    login_manager = LoginManager()
-    login_manager.init_app(flaskApp)
-    login_manager.login_view = "login"
+# Login manager
+login_manager = LoginManager()
+login_manager.init_app(flaskApp)
+login_manager.login_view = "login"
 
-    # Import routes and models at the end to avoid circular imports
-    from app import routes, models
+# Import routes and models at the end to avoid circular imports
+from app import routes, models
 
-    # Register blueprints
-    from app.main_bp import main_bp
-    flaskApp.register_blueprint(main_bp)
+# Initialise database on startup (if it doesnt exist).
+models.init_db()
 
-    # Initialise database on startup (if it doesnt exist).
-    models.init_db()
-
-    #User loader function
-    @login_manager.user_loader
-    def load_user(userID):
-        return models.Users.query.get(int(userID))
-
-    return flaskApp
+# User loader function
+@login_manager.user_loader
+def load_user(userID):
+    return models.Users.query.get(int(userID))
