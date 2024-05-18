c730c117b4c5377ad0db7a642fa3b38952d0caec
UpstandingVlad <164867745+UpstandingVlad@users.noreply.github.com>
2024-05-18 23:48:56 +0800

    Attempted changes but get error: Detected factory 'create_app' in module 'app', but could not call it without arguments.

--- src/QuestersCache.py
+++ src/QuestersCache.py
@@ -1,19 +1,12 @@
 # Imports
-from app import create_app
-from app.config import TestConfig, DeploymentConfig
+from app import create_app, db
+from app.config import TestConfig
 from app import models
 from app import models
 
 # Main application code
 
 # Create the Flask app
-app = create_app(TestConfig)
-
-# Main application code
-if __name__ == "__main__":
-    # Add any additional setup or initialization code here
-    # For example, you might want to initialize the database
-    # models.init_db()
-
-    # Run the Flask app
-    app.run()
+print("Creating app with testconfig")
+flaskApp = create_app(TestConfig)
+migrate = Migrate(flaskApp, db)
--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,31 +3,44 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.config import Config
+from app.config import Config, TestConfig
 
-# Main application name
-flaskApp = Flask(__name__, template_folder='../templates', static_folder='../static')
+db = SQLAlchemy()
 
-# Configuration
-flaskApp.config.from_object(Config)
-flaskApp.debug = True # Debug error messages
+def create_app(config):
+    # Main application name
+    flaskApp = Flask(__name__, template_folder='../templates', static_folder='../static')
 
-# Initialise extensions
-db = SQLAlchemy(flaskApp)
-migrate = Migrate(flaskApp, db)
+    # Configuration
+    #flaskApp = Flask(__name__)
+    flaskApp.config.from_object(config)
+    #flaskApp.debug = True # Debug error messages
 
-# Login manager
-login_manager = LoginManager()
-login_manager.init_app(flaskApp)
-login_manager.login_view = "login"
+    db.init_app(flaskApp)
+   
+    
+    # Initialise extensions
+    #db = SQLAlchemy(flaskApp)
+    #migrate = Migrate(flaskApp, db)
+    
+    # Login manager
+    login_manager = LoginManager()
+    login_manager.init_app(flaskApp)
+    login_manager.login_view = "login"
 
-# Import routes and models at the end to avoid circular imports
-from app import routes, models
+    # Import routes and models at the end to avoid circular imports
+    from app import routes, models
 
-# Initialise database on startup (if it doesnt exist).
-models.init_db()
+    # Register blueprints
+    from app.main_bp import main_bp
+    flaskApp.register_blueprint(main_bp)
 
-# User loader function
-@login_manager.user_loader
-def load_user(userID):
-    return models.Users.query.get(int(userID))
+    # Initialise database on startup (if it doesnt exist).
+    models.init_db()
+
+    #User loader function
+    @login_manager.user_loader
+    def load_user(userID):
+        return models.Users.query.get(int(userID))
+
+    return flaskApp
