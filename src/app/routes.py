0c4dd6e969f803a3165bcd7bc025345e657534c6
UpstandingVlad <164867745+UpstandingVlad@users.noreply.github.com>
2024-05-18 19:52:55 +0800

    attempting new fix querying user count

--- src/app/routes.py
+++ src/app/routes.py
@@ -236,7 +236,7 @@ def quest_view():
 def leaderboard():
     
     # Variable defines users per page on leaderboard
-    page_size = 50
+    page_size = 10
     # Get the current page number
     page_number = int(request.args.get("page", 1))  
    
@@ -249,10 +249,10 @@ def leaderboard():
     
     # calcualte which page number is prev and next (if they exist)
     prev_page = page_number - 1 if page_number > 1 else None
-    next_page = page_number + 1 if end_index < len(leaderboard_users) else None
+    next_page = page_number + 1 if end_index < Users.query.count() else None
     
     # calculate total users and total pages
-    total_users = len(leaderboard_users)
+    total_users = Users.query.count()
     total_pages = (total_users + page_size - 1) // page_size
     
     # set the current page number
