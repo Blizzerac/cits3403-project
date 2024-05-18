71a24b7f81e2bc5a6821352bd2511a103ac00c60
UpstandingVlad <164867745+UpstandingVlad@users.noreply.github.com>
2024-05-19 01:19:36 +0800

    idk whats going on

--- src/QuestersCache.py
+++ src/QuestersCache.py
@@ -1,4 +1,8 @@
 # Imports
 from app import models
+from app import create_app, db
+from app.config import DeploymentConfig
 
 # Main application code
+flaskApp = create_app(DeploymentConfig)
+migrate = Migrate(db, flaskApp)
--- src/app/__init__.py
+++ src/app/__init__.py
@@ -5,21 +5,31 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app.config import Config
 
-# Main application name
-flaskApp = Flask(__name__, template_folder='../templates', static_folder='../static')
+db = SQLAlchemy()
 
-# Configuration
-flaskApp.config.from_object(Config)
-debug = True # Debug error messages
+def create_app(config):
+    # Main application name
+    flaskApp = Flask(__name__, template_folder='../templates', static_folder='../static')
 
-# Initialise extensions
-db = SQLAlchemy(flaskApp)
-migrate = Migrate(flaskApp, db)
+    # Configuration
+    flaskApp.config.from_object(Config)
+    #debug = True # Debug error messages
 
-# Login manager
-login_manager = LoginManager()
-login_manager.init_app(flaskApp)
-login_manager.login_view = "login"
+    db.init_app(flaskApp)
+
+    from app.blueprints import main
+    flaskApp.register_blueprint(main)
+
+    # Initialise extensions
+    #db = SQLAlchemy(flaskApp)
+    #migrate = Migrate(flaskApp, db)
+
+    # Login manager
+    login_manager = LoginManager()
+    login_manager.init_app(flaskApp)
+    login_manager.login_view = "login"
+    
+    return flaskApp
 
 # Import routes and models at the end to avoid circular imports
 from app import routes, models
@@ -30,4 +40,4 @@ models.init_db()
 # User loader function
 @login_manager.user_loader
 def load_user(userID):
-    return models.Users.query.get(int(userID))
+        return models.Users.query.get(int(userID))
